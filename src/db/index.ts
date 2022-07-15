d6e2eed948a5bd725a85f1f35bb9fb1bab73aa74
Andrew Dame <andrew_dame@outlook.com>
2022-07-15 13:55:56 -0400

    feat: removes all db env variables and replace with just one (db url)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,13 @@
 import knex from "knex";
 import { attachPaginate } from "knex-paginate";
 
-const { DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_NAME } = process.env;
+const { DATABASE_URL } = process.env;
 
 const dbClient = knex({
   client: "pg",
   version: "13.6",
   pool: {min: 0, max: 100},
-  connection: {
-    host: DB_HOSTNAME,
-    port: Number(DB_PORT),
-    user: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-  }
+  connection: DATABASE_URL
 });
 
 attachPaginate();
