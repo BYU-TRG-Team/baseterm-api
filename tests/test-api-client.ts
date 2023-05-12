481186cd519bb8008f60987022c12461851d9d47
Andrew Dame <andrew_dame@outlook.com>
2023-05-11 21:26:02 -0400

    chore: adds comments for test helper functions

--- tests/test-api-client.ts
+++ tests/test-api-client.ts
@@ -1,11 +1,10 @@
 import supertest from "supertest";
-import { v4 as uuid } from "uuid";
 import { Role, AuthToken } from "@byu-trg/express-user-management";
 import jwt from "jsonwebtoken";
 
 export const TEST_API_AUTH_COOKIE_NAME = "TRG_AUTH_TOKEN";
 export const TEST_API_AUTH_SECRET = "killjoy";
-export const TEST_USER_ID = uuid();
+export const TEST_USER_ID = "c0e29bfc-279b-4235-b301-c5e439da1e15";
 export const TEST_AUTH_TOKEN = jwt.sign({
   id: TEST_USER_ID,
   role: Role.Admin,
